1b2467af5bbee49c113defa1198c917d479defbb
swhan <swhan@PS-3D>
2022-07-21 17:17:31 +0900

    초기 커밋.

---
+++ SampleCode/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SampleCode
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            // Note: it is not best practice to store API keys in source code.
+            // The API key is referenced here for the convenience of this tutorial.
+            Esri.ArcGISRuntime.ArcGISRuntimeEnvironment.ApiKey = "AAPKa74da13764ce47b789a642c5d24f4a5c09yD0JnO_HgdsZ4aUDH8R83nOcWJ3huVb5pl8_Nn5HahwZN5fjrpaPko5ap-ru8t";
+        }
+    }
+}
---
+++ SampleCode/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+
+namespace SampleCode
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MapPoint mapCenterPoint = new MapPoint(-118.805, 34.027, SpatialReferences.Wgs84);
+            MainMapView.SetViewpoint(new Viewpoint(mapCenterPoint, 100000));
+        }
+    }
+}
