2a58fd4a2eecc59c35100a2fb126067ce209bff1
swhan <swhan@esrikr.com>
2022-07-22 10:18:27 +0900

    Add traffic layer

--- SampleCode/MapViewModel.cs
+++ SampleCode/MapViewModel.cs
@@ -37,8 +37,23 @@ namespace SampleCode
         private void SetupMap()
         {
 
+            /* Display a Map 
             // Create a new map with a 'topographic vector' basemap.
             Map = new Map(BasemapStyle.ArcGISTopographic);
+            */
+
+            // Create a new map with a 'topographic vector' basemap.
+            var trafficMap = new Map(BasemapStyle.ArcGISTopographic);
+
+            // Create a layer to display the ArcGIS World Traffic service.
+            var trafficServiceUrl = "https://traffic.arcgis.com/arcgis/rest/services/World/Traffic/MapServer";
+            var trafficLayer = new ArcGISMapImageLayer(new Uri(trafficServiceUrl));
+
+            // Add the traffic layer to the map's data layer collection.
+            trafficMap.OperationalLayers.Add(trafficLayer);
+
+            // Set the view model Map property with the new map.
+            this.Map = trafficMap;
 
         }
     }
